e45ba1f3b950a037fa3ed4d83d37fc9b26623c95
Garvit Singla <garvits093@gmail.com>
2026-01-03 21:02:49 +0530

    stacks started

--- Stacksandqueues/implementations/stackUsingArrays.cpp
+++ Stacksandqueues/implementations/stackUsingArrays.cpp
@@ -1,3 +1,34 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+class MyStack{
+    int* arr;
+    int top;
+    int capacity;
+    public:
+        MyStack(int size=10){
+            arr = new int[size];
+            capacity = size;
+            top = -1;
+        }
+        ~MyStack(){
+            delete[] arr;
+        }
+        void push(int x) {
+        if (top >= capacity - 1) {
+            cout << "Stack overflow" << endl;
+            return;
+        }
+        arr[++top] = x;
+        }
+        int pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            // Return invalid value
+            return -1; 
+        }
+        return arr[top--];
+    }
+
+};
